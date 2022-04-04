41c28d6198f3b6cd3b047fb772f3b9fda53cd9e3
hanah <hanah.benghz@gmail.com>
2022-04-04 14:18:20 +0100

    added user unit tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,50 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { CreateUserDto } from './dto/create-user-dto';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let spyService: UsersService;
 
   beforeEach(async () => {
+    const ApiServiceProvider = {
+      provide: UsersService,
+      useFactory: () => ({
+        create: jest.fn(() => {}),
+        findOne: jest.fn(() => {}),
+        findAll: jest.fn(() => []),
+      }),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService, ApiServiceProvider],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    spyService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be calling create method', () => {
+    const userDto = new CreateUserDto();
+    controller.create(userDto);
+    expect(spyService.create).toHaveBeenCalled();
+    expect(spyService.create).toHaveBeenCalledWith(userDto);
+  });
+
+  it('should be calling findAll method', () => {
+    controller.findAll();
+    expect(spyService.findAll).toHaveBeenCalled();
+  });
+
+  it('should be calling findOne method', () => {
+    const id = '12';
+    controller.findOne(id);
+    expect(spyService.findOne).toHaveBeenCalled();
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,27 @@
+import { CreateUserDto } from './dto/create-user-dto';
+import { Users } from './users.entity';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import * as sinon from 'sinon';
+import { Repository } from 'typeorm';
+
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let sandbox: sinon.SinonSandbox;
 
   beforeEach(async () => {
+    sandbox = sinon.createSandbox();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(Users),
+          useValue: sinon.createStubInstance(Repository),
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +29,25 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should call create method with expected params', async () => {
+    const createUserSpy = jest.spyOn(service, 'create');
+    const dto = new CreateUserDto();
+    service.create(dto);
+    expect(createUserSpy).toHaveBeenCalledWith(dto);
+  });
+
+  it('should call findOne method with expected params', async () => {
+    const findOneUserSpy = jest.spyOn(service, 'findOne');
+    const id = 12;
+    service.findOne(id);
+    expect(findOneUserSpy).toHaveBeenCalledWith(id);
+  });
+
+  it('should call findAll method with expected params', async () => {
+    const findAllUserSpy = jest.spyOn(service, 'findAll');
+    service.findAll();
+    expect(findAllUserSpy).toHaveBeenCalledWith();
   });
 });
